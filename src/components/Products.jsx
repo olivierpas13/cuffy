f815842d87d67c66b155d46d4191f860bf6c86c0
Olivier Paspuel <olivier_paspuel@hotmail.com>
2023-04-20 18:56:05 -0500

    feat: products view now displays all the products and has a filter and search functionality

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,42 +1,132 @@
 import products from "@/lists/products";
+import Image from "next/image";
+import { useState } from "react";
 
 const Products = () => {
+  const [search, setSearch] = useState("");
+  const [selectedProperty, setSelectedProperty] = useState("");
+  const [selectedStatus, setSelectedStatus] = useState("");
+
+  const handleSearchChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const handlePropertyChange = (e) => {
+    setSelectedProperty(e.target.value);
+  };
+
+  const handleStatusChange = (e) => {
+    setSelectedStatus(e.target.value);
+  };
+
+  const filteredProducts = products.filter((product) => {
+    return (
+      (product.name.toLowerCase().includes(search.toLowerCase()) ||
+        product.description.toLowerCase().includes(search.toLowerCase())) &&
+      (selectedProperty === "" || product.properties.includes(selectedProperty)) &&
+      (selectedStatus === "" || product.status === selectedStatus)
+    );
+  });
+
+  const allProperties = [...new Set(products.flatMap((product) => product.properties))];
+  const allStatuses = [...new Set(products.map((product) => product.status))];
+
   return (
-    <div className="container mx-auto" >
-            <h1 className="text-3xl font-bold p-10">Todos los productos</h1>
-            <div class="flex flex-wrap">
-      {products.map((product) => {
-        return (
-          <div key={product.id} className="card w-96 bg-base-100 max-h-[40rem] grow m-4 shadow-xl">
-            <figure>
-              <img
-                src={`/products/${product.id}.jpg`}
-                alt={product.name}
-              />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title">
-                {product.name}
-                <div className="badge badge-secondary">{product.status}</div>
-              </h2>
-              <p>{product.description}</p>
-              <div className="card-actions justify-end">
-                {product.properties.map((property) => {
-                  return (
-                    <div key={property} className="badge badge-outline">
-                      {property}
-                    </div>
-                  );
-                })}
+    <div className="container mx-auto px-4 py-8">
+      <div className="mb-8">
+        <h1 className="text-3xl font-bold mb-4">Todos los productos</h1>
+        <div className="flex items-center flex-wrap gap-4">
+          <div className="mx-3" >
+            <h3 className="text-lg font-medium mb-2">Filtrar por propiedad:</h3>
+            <select
+              className="border border-gray-300 rounded-md px-2 py-1"
+              onChange={handlePropertyChange}
+              value={selectedProperty}
+            >
+              <option value="">Todos</option>
+              {allProperties.map((property) => (
+                <option key={property} value={property}>
+                  {property.charAt(0).toUpperCase() + property.slice(1)}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div>
+            <h3 className="text-lg font-medium mb-2">Filtrar por estado:</h3>
+            <select
+              className="border border-gray-300 rounded-md px-2 py-1"
+              onChange={handleStatusChange}
+              value={selectedStatus}
+            >
+              <option value="">Todos</option>
+              {allStatuses.map((status) => (
+                <option key={status} value={status}>
+                  {status}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="mx-20">
+            <h3 className="text-lg font-medium mb-2">Buscar:</h3>
+            <input
+              type="text"
+              className="border border-gray-300 rounded-md px-4 py-1"
+              value={search}
+              onChange={handleSearchChange}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-8">
+        {filteredProducts.map((product) => {
+          return (
+            <div
+              key={product.id}
+              className="bg-white rounded-md shadow-md overflow-hidden"
+            >
+              <div className="relative h-56 md:h-64 lg:h-96">
+                <Image
+                  src={`/products/${product.id}.jpeg`}
+                  alt={product.name}
+                  fill
+                />
               </div>
+              <div className="px-4 py-3">
+                <h2 className="text-lg font-medium">{product.name}</h2>
+               
+                <p className="text-sm text-gray-500 mb-2">
+              {product.description}
+            </p>
+            <div className="flex flex-wrap gap-2">
+              {product.properties.map((property) => (
+                <div
+                  key={property}
+                  className="bg-gray-100 text-gray-600 rounded-full px-2 py-1 text-xs"
+                >
+                  {property}
+                </div>
+              ))}
+            </div>
+            <div className="mt-2">
+              <span
+                className={`inline-block px-2 py-1 rounded-md text-sm font-medium ${
+                  product.status === "En stock"
+                    ? "bg-green-100 text-green-800"
+                    : product.status === "Más vendido"
+                    ? "bg-yellow-100 text-yellow-800"
+                    : "bg-red-100 text-red-800"
+                }`}
+              >
+                {product.status}
+              </span>
             </div>
           </div>
-        );
-      })}
-            </div>
-
-    </div>
-  );
+        </div>
+      );
+    })}
+  </div>
+</div>
+);
 };
 
 export default Products;
--- src/lists/products.js
+++ src/lists/products.js
@@ -1,49 +1,117 @@
-const products= [
+const products = [
     {
-        id: "1",
-        name: "Rosa",
-        description: "Rosa tejida de 30cm de largo, colores a elección",  
-        properties: [
-            "flor"
-        ],
-        status: "En stock"
+      id: "1",
+      name: "Planta Piraña",
+      description:
+        "Planta Piraña tejida de 30cm de largo, en honor a Mario Bros. Recientemente, Bowser sostuvo un ramo de estas plantas en la película.",
+      properties: ["flor", "mario"],
+      status: "Más vendido",
     },
     {
-        id: "2",
-        name: "Girasol",
-        description: "Girasol tejida de 30cm de largo, colores a elección",  
-        properties: [
-            "flor"
-        ],
-        status: "Más vendido"
+      id: "2",
+      name: "Llavero de Perrito",
+      description:
+        "Llavero tejido en forma de perrito, perfecto para llevar tus llaves con estilo.",
+      properties: ["animal", "llavero"],
+      status: "En stock",
     },
     {
-        id: "3",
-        name: "Clavel",
-        description: "Clavel tejida de 30cm de largo, colores a elección",  
-        properties: [
-            "flor"
-        ],
-        status: "Agotado"
+      id: "3",
+      name: "Mini Maceta de Girasol",
+      description:
+        "Mini maceta de girasol tejida a mano, perfecta para darle un toque de alegría a tu hogar.",
+      properties: ["flor", "mini"],
+      status: "En stock",
     },
     {
-        id: "4",
-        name: "Tulipan",
-        description: "Tulipan tejida de 30cm de largo, colores a elección",  
-        properties: [
-            "flor"
-        ],
-        status: "En stock"
+      id: "4",
+      name: "Muñeco de Anya Forger",
+      description:
+        "Muñeco tejido de Anya Forger, uno de los personajes principales de Spy x Family.",
+      properties: ["anime", "muñeco"],
+      status: "En stock",
     },
     {
-        id: "4",
-        name: "Tulipan",
-        description: "Tulipan tejida de 30cm de largo, colores a elección",  
-        properties: [
-            "flor"
-        ],
-        status: "En stock"
+      id: "5",
+      name: "Clavel",
+      description:
+        "Clavel tejido de 30cm de largo, disponible en cualquier color que desees.",
+      properties: ["flor"],
+      status: "En stock",
     },
-]
+    {
+      id: "6",
+      name: "Mini Ramo de Rosas",
+      description:
+        "Mini ramo de rosas tejidas a mano, perfecto para regalar a alguien especial.",
+      properties: ["flor", "mini"],
+      status: "En stock",
+    },
+    {
+      id: "7",
+      name: "Mini Maceta de Flores",
+      description:
+        "Mini maceta de flores tejida a mano, perfecta para decorar cualquier espacio.",
+      properties: ["flor", "mini"],
+      status: "En stock",
+    },
+    {
+      id: "8",
+      name: "Sombrero Nube para Bebé",
+      description:
+        "Sombrero tejido en forma de nube para bebé, suave y cómodo para proteger la cabeza del bebé del sol.",
+      properties: ["ropa", "bebé"],
+      status: "Agotado",
+    },
+    {
+      id: "9",
+      name: "Rosa",
+      description:
+        "Rosa tejida de 30cm de largo, disponible en cualquier color que desees.",
+      properties: ["flor"],
+      status: "En stock",
+    },
+    {
+      id: "10",
+      name: "Saco Osito para Bebé",
+      description:
+        "Saco tejido en forma de osito para bebé, suave y cómodo para mantener al bebé abrigado.",
+      properties: ["animal", "ropa", "bebé"],
+      status: "En stock",
+    },
+    {
+      id: "11",
+      name: "Llavero de Perrito",
+      description:
+        "Llavero tejido en forma de perrito, perfecto para llevar tus llaves con estilo.",
+      properties: ["animal", "llavero"],
+      status: "En stock",
+    },
+    {
+      id: "12",
+      name: "Llavero de la Mona Lisa",
+      properties: ["llavero"],
+      description:
+        "Llavero tejido en forma de la Mona Lisa, para los amantes del arte",
+      status: "En stock",
 
-export default products;
+    },
+    {
+    id: "13",
+    name: "Llavero de Gatito",
+    description:
+    "Llavero tejido en forma de gatito, perfecto para llevar tus llaves con estilo.",
+    properties: ["animal", "llavero"],
+    status: "En stock",
+    },
+    {
+    id: "14",
+    name: "Cartucho",
+    description:
+    "Cartucho tejido en forma de flor, perfecto para decorar tu hogar.",
+    properties: ["flor"],
+    status: "Más vendido",
+    },
+    ];
+    
+    export default products;  
