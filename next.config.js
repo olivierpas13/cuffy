9129c2fab6bff701dcef5680d31b69bde0d0b504
Olivier Paspuel <olivier_paspuel@hotmail.com>
2024-03-23 18:09:46 -0500

    fix nextconfig file

--- next.config.js
+++ next.config.js
@@ -4,20 +4,28 @@ const nextConfig = {
   images: {
     domains: ["res.cloudinary.com"],
   },
-  async headers() {
+  async rewrites(){
     return [
       {
-        // matching all API routes
-        source: "*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "https://cuffy-backend.vercel.app" }, // replace this your actual origin
-          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
+        source: "/",
+        destination: "https://cuffy-backend.vercel.app/*"
       }
     ]
   }
+  // async headers() {
+  //   return [
+  //     {
+  //       // matching all API routes
+  //       source: "/",
+  //       headers: [
+  //         { key: "Access-Control-Allow-Credentials", value: "true" },
+  //         { key: "Access-Control-Allow-Origin", value: "https://cuffy-backend.vercel.app" }, // replace this your actual origin
+  //         { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+  //         { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+  //       ]
+  //     }
+  //   ]
+  // }
 };
 
 module.exports = nextConfig;
